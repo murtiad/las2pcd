880728d8febc1b0175aa5ba92bc9c5e23dc8a7d3
Jonathan Greenberg <jgreenberg@unr.edu>
2018-03-16 17:28:17 -0700

    char fix

--- las2pcd.cpp
+++ las2pcd.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int main (int argc, char** argv[])
+int main (int argc, char *argv[])
 {
 	string filePath;
 	cout << "===================================================================" << endl;
